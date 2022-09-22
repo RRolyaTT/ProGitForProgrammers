32b218c3e9548a10935d5aacb2a5f93fe510196e
RRolyaTT <2002415@uad.ac.uk>
2022-09-22 12:31:47 +0100

    Add writeline indicating we are in command line

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I jsut added this in visual studio ajfbijfb w");
+Console.WriteLine("Command Line aibfai agabg");
